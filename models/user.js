b45636f67c52d3ab9f54386fb7c735ae6a354bf4
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-09 09:12:59 -0800

    added password security with bcrypt.js

--- models/user.js
+++ models/user.js
@@ -46,11 +46,16 @@ module.exports = (sequelize) => {
             },
         },
         password: {
-            // Use a virtual field that only Sequelize populates but doesn't get inserted as a column in the DB table,
-            // this way we don't store un un-hashed password in the DB
-            // type: DataTypes.VIRTUAL,
             type: DataTypes.STRING,
             allowNull: false,
+            // Defining a custom setter for the model with the set() method
+            // set() receives the value, val, to set the password field
+            set(val){
+                // hash the password with bcrypt.hashSync()
+                const hashedPassword = bcrypt.hashSync(val, 10);
+                // setDataValue() is a Sequelize method used inside setters to update the underlying data value
+                this.setDataValue('password', hashedPassword);
+            },
             validate: {
                 notNull: {
                     msg: 'A password is required'
@@ -60,41 +65,6 @@ module.exports = (sequelize) => {
                 },
             },
         },
-
-
-/**
- * I WASN"T ABLE TO TEST PASSWORD SECURITY! 
- * Getting error: 
- *     "message": "SQLITE_ERROR: no such column: confirmedPassword",
-    "error": {}
- */
-
-
-        // confirmedPassword: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false,
-        //     // Defining a custom setter for the model with the set() method
-        //     // set() receives the value, val, to set the confirmedPassword
-        //     set(val){
-        //         // conditional to see if the values are the same...
-        //         if ( val === this.password ) {
-        //             // hash the confirmed password with bcrypt.hashSync()
-        //             const hashedPassword = bcrypt.hashSync(val, 10);
-        //             // setDataValue() is a Sequelize method used inside setters to update the underlying data value
-        //             this.setDataValue('confirmedPassword', hashedPassword);
-        //         }
-        //     },
-        //     validate: {
-        //         notNull: {
-        //             msg: 'Both passwords must match'
-        //         }
-        //     },
-        // }
-
-
-
-
-        
     }, { sequelize });
     
     User.associate = (models) => {
