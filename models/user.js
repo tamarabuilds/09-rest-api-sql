7e69c6904e9c3ca3700f26324404154374e4c68f
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-08 18:25:34 -0800

    commented out password security

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize) => {
     class User extends Model {}
@@ -45,6 +46,9 @@ module.exports = (sequelize) => {
             },
         },
         password: {
+            // Use a virtual field that only Sequelize populates but doesn't get inserted as a column in the DB table,
+            // this way we don't store un un-hashed password in the DB
+            // type: DataTypes.VIRTUAL,
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
@@ -56,6 +60,41 @@ module.exports = (sequelize) => {
                 },
             },
         },
+
+
+/**
+ * I WASN"T ABLE TO TEST PASSWORD SECURITY! 
+ * Getting error: 
+ *     "message": "SQLITE_ERROR: no such column: confirmedPassword",
+    "error": {}
+ */
+
+
+        // confirmedPassword: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     // Defining a custom setter for the model with the set() method
+        //     // set() receives the value, val, to set the confirmedPassword
+        //     set(val){
+        //         // conditional to see if the values are the same...
+        //         if ( val === this.password ) {
+        //             // hash the confirmed password with bcrypt.hashSync()
+        //             const hashedPassword = bcrypt.hashSync(val, 10);
+        //             // setDataValue() is a Sequelize method used inside setters to update the underlying data value
+        //             this.setDataValue('confirmedPassword', hashedPassword);
+        //         }
+        //     },
+        //     validate: {
+        //         notNull: {
+        //             msg: 'Both passwords must match'
+        //         }
+        //     },
+        // }
+
+
+
+
+        
     }, { sequelize });
     
     User.associate = (models) => {
